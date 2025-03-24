47a3fdb303b1cab5a0fd70ffdc74b55f3a31c82d
Juerson <zhangrson@163.com>
2025-03-24 19:07:29 +0800

    update

--- src/utils/indent.rs
+++ src/utils/indent.rs
@@ -1,30 +1,20 @@
 use yaml_rust::{ YamlEmitter, YamlLoader };
 
-/// 调整 YAML 缩进
+#[allow(dead_code)]
+/// 调整YAML缩进
 pub fn adjust_yaml_indentation(yaml_str: &str) -> String {
-    // 尝试加载和处理 YAML 数据
-    let data = match YamlLoader::load_from_str(yaml_str) {
-        Ok(data) => data,
-        Err(_) => {
-            return "Error: Failed to parse YAML input".to_string();
+    let data = YamlLoader::load_from_str(yaml_str).ok();
+    let doc = data.as_ref().and_then(|d| d.first());
+
+    let mut output = String::new();
+    if let Some(doc) = doc {
+        let mut emitter = YamlEmitter::new(&mut output);
+        if emitter.dump(doc).is_err() {
+            return "Error: Failed to emit YAML".to_string();
         }
-    };
-    // 获取第一个文档
-    let doc = if let Some(doc) = data.get(0) {
-        doc
     } else {
         return "Error: No document found in YAML input".to_string();
-    };
-    let mut output = String::new();
-    let mut emitter = YamlEmitter::new(&mut output);
-    // 尝试将文档转储到输出字符串
-    if emitter.dump(doc).is_ok() {
-        // 去掉开头的 `---\n`（如果存在的话）
-        if output.starts_with("---\n") {
-            return output[4..].to_string();
-        }
-        return output;
-    } else {
-        return "Error: Failed to emit YAML".to_string();
     }
+
+    output.strip_prefix("---\n").unwrap_or(&output).to_string()
 }
